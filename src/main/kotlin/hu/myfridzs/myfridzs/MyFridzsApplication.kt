48c5fe045c5ec7ca76b328841cfa3a55fe0b1d69
Kálvin Tamás <kalvintamasendre@gmail.com>
2023-05-24 13:15:39 +0200

    Initial commit

---
+++ src/main/kotlin/hu/myfridzs/myfridzs/MyFridzsApplication.kt
@@ -0,0 +1,12 @@
+package hu.myfridzs.myfridzs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyFridzsApplication
+
+fun main(args: Array<String>)
+{
+    runApplication<MyFridzsApplication>(*args)
+}
---
+++ src/test/kotlin/hu/myfridzs/myfridzs/MyFridzsApplicationTests.kt
@@ -0,0 +1,15 @@
+package hu.myfridzs.myfridzs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyFridzsApplicationTests
+{
+
+    @Test
+    fun contextLoads()
+    {
+    }
+
+}
